a9eaf261b2300d66d59e318560084e38767708bc
Prajas <prajaswadekar@gmail.com>
2023-04-20 12:06:28 +0530

    new: deployable

--- mail_chat/settings.py
+++ mail_chat/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mail_chat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
