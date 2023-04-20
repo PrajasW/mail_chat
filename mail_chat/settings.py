35ac91e2871158be220c18fdfb9dc440f423eb30
Prajas <prajaswadekar@gmail.com>
2023-04-20 11:52:08 +0530

    try: deploy ready?

--- mail_chat/settings.py
+++ mail_chat/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- user_auth/models.py
+++ user_auth/models.py
@@ -9,8 +9,8 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    def save(self):
-        super().save()
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.profile_pic.path)
         if img.height > 640 or img.width > 640:
             output_size = (640,640)
